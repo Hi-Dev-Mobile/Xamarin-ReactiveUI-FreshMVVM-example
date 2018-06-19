811adc17a4076f1148e4518a52dc159a8ddd751d
k2 <k2interactivestudios@gmail.com>
2018-06-19 02:49:05 -0700

    finish rxifiying

--- AutoskillTestRun/PageModels/QuoteListPageModel.cs
+++ AutoskillTestRun/PageModels/QuoteListPageModel.cs
@@ -1,73 +1,60 @@
-﻿using System;
-using System.Collections.ObjectModel;
-using FreshMvvm;
-using PropertyChanged;
-using AutoskillTestRun.Services;
-using AutoskillTestRun.Models;
+﻿using System.Collections.ObjectModel;
+using ReactiveUI;
 using Xamarin.Forms;
+
+using AutoskillTestRun.Models;
 using AutoskillTestRun.Pages;
+using AutoskillTestRun.Services;
 
 namespace AutoskillTestRun. PageModels
 {
-	[AddINotifyPropertyChangedInterface]
-	public class QuoteListPageModel: FreshBasePageModel
+	public class QuoteListPageModel: BasePageModel
     {
 		IDatabaseService databaseService;
 
 		public ObservableCollection<Quote> Quotes { get; set; }
 
+		public ReactiveCommand AddCommand { get; private set; }
+		public ReactiveCommand SelectedCommand { get; private set; }
+
 		Quote selectedQuote;
 		public Quote SelectedQuote {
 			get => selectedQuote;
-			set {
-				selectedQuote = value;
-				if (value != null)
-					QuoteSelected. Execute ( selectedQuote );
-			}
+			set => this. RaiseAndSetIfChanged ( ref selectedQuote, value );
 		}
 
-		public Command AddQuote {
-			get => new Command ( async () => await CoreMethods. PushPageModel<QuotePageModel> () );
-		}
 
-		public Command<Quote> QuoteSelected {
-			get => new Command<Quote> ( async ( quote ) => {
 
-				var page = this. CurrentPage as QuoteListPage;
-				page. DeselectListView ();
-				await CoreMethods. PushPageModel<QuotePageModel> ( quote );
-            } );
-		}
-
-		
-        // init
 		public QuoteListPageModel (IDatabaseService databaseService)
         {
 			this.databaseService = databaseService;
+			Quotes = new ObservableCollection<Quote> ( databaseService. GetQuotes () );         
+
+            AddCommand = ReactiveCommand
+                . Create ( async () => await CoreMethods. PushPageModel<QuotePageModel> () );
+
+            SelectedCommand = ReactiveCommand
+                . Create<SelectedItemChangedEventArgs> ( SelectedAction );
+        }
+  
+        
+        public override void ReverseInit ( object value )
+        {
+            var newQuote = value as Quote;
+
+            if (!Quotes. Contains ( newQuote ))
+                Quotes. Add ( newQuote );
         }
 
-		public override void Init ( object initData )
+
+		async void SelectedAction(SelectedItemChangedEventArgs args)
 		{
-			Quotes = new ObservableCollection<Quote> ( databaseService. GetQuotes () );         
-		}
-
-
-		//protected override void ViewIsAppearing ( object sender, EventArgs e )
-		//{
-		//	base. ViewIsAppearing ( sender, e );
-		//}      
-		//protected override void ViewIsDisappearing ( object sender, EventArgs e )
-		//{
-		//	base. ViewIsDisappearing ( sender, e );
-		//}
-
-
-		public override void ReverseInit ( object value )
-		{
-			var newQuote = value as Quote;
-
-			if (!Quotes. Contains ( newQuote ))
-				Quotes. Add ( newQuote );
+			var quote = args. SelectedItem as Quote;
+			if (quote == null) return;
+			
+			var page = this. CurrentPage as QuoteListPage;
+			page. DeselectListView ();
+			await CoreMethods. PushPageModel<QuotePageModel> ( quote );
 		}
 	}
 }
